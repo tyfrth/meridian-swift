f268228b55e2ae38ea38618eddfebc375b573f93
tyfrth <tyfrith@gmail.com>
2018-07-09 17:14:43 -0700

    set up

---
+++ meridian-swift/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  meridian-swift
+//
+//  Created by Tyler Frith on 7/9/18.
+//  Copyright © 2018 tyfrith. All rights reserved.
+//
+
+import UIKit
+import Meridian
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
